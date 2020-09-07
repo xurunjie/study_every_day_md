fb2e6352d409e8748b9c3dee0ff98dacda7063bf
xurunjie <38467446+xurunjie@users.noreply.github.com>
2020-09-07 15:09:40 +0800

    广度优先深度优先算法

--- 数据结构与算法/DataStructure/src/com/kedacom/graph/Graph.java
+++ 数据结构与算法/DataStructure/src/com/kedacom/graph/Graph.java
@@ -2,6 +2,7 @@ package com.kedacom.graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * @author python
@@ -23,10 +24,15 @@ public class Graph {
      */
     private int numOfEdges;
 
+    /**
+     * 定义数组标注是否被访问
+     */
+    private boolean[] isVisited;
+
     public static void main(String[] args) {
         // 存在5个顶点
         int n = 5;
-        String[] vertexValue = {"A","B","C","D","E"};
+        String[] vertexValue = {"A", "B", "C", "D", "E"};
         // 创建图对象
         Graph graph = new Graph(n);
         // 循环的添加顶点
@@ -35,14 +41,20 @@ public class Graph {
         }
 
         // 添加边
-        graph.insertEdge(0,1,1);
-        graph.insertEdge(0,2,1);
-        graph.insertEdge(1,2,1);
-        graph.insertEdge(1,3,1);
-        graph.insertEdge(1,4,1);
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
         // 显示邻接举证
         graph.showGraph();
+        // 深度遍历
+        System.out.println("深度遍历 -> ");
+        graph.dfs();
 
+
+        // 广度优先
+        System.out.println("广度遍历 -> ");
+        graph.bfs();
     }
 
     /**
@@ -58,6 +70,138 @@ public class Graph {
     }
 
     /**
+     * 得到第一个邻接点的下标 w
+     *
+     * @param index index
+     * @return 如果存在就返回对应的下标如果不存在则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (edges[index][i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标来获取下一个邻接节点
+     *
+     * @param v1 v1
+     * @param v2 v2
+     * @return 返回下一个下标
+     */
+    public int getNextNeighbor(int v1, int v2) {
+        for (int i = v2 + 1; i < vertexList.size(); i++) {
+            if (edges[v1][i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @param i 索引
+     * @return 返回类型
+     */
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    /**
+     * 深度优先算法
+     *
+     * @param i         第一次是0
+     * @param isVisited 是否被访问数组
+     */
+    public void dfs(boolean[] isVisited, int i) {
+        // 首先我们访问该节点,输出
+        System.out.print(getValueByIndex(i) + "->");
+        // 将节点设置为已访问
+        isVisited[i] = true;
+        // 查找节点i的邻接点w
+        int w = getFirstNeighbor(i);
+
+        while (w != -1) {
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            // 如果w已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+
+    /**
+     * 广度遍历bfs
+     * @param isVisited 访问数组
+     * @param i i
+     */
+    private void bfs(boolean[] isVisited, int i) {
+        // 表示队列的头节点对应的下标
+        int u;
+        // 邻接点w
+        int w;
+        // 队列,记录节点访问的顺序
+        LinkedList<Integer> queue = new LinkedList<>();
+        // 访问节点,输出节点信息
+        System.out.print(getValueByIndex(i) + "->");
+        // 标记为已访问
+        isVisited[i] = true;
+        // 将节点加入队列
+        queue.addLast(i);
+
+        while (!queue.isEmpty()) {
+            // 取出队列的头结点下标
+            u = queue.removeFirst();
+            // 得到第一个邻接点的下标
+            w = getFirstNeighbor(u);
+            // w!=-1
+            while (w != -1) {
+                // 是否被访问
+                if (!isVisited[w]) {
+                    System.out.println(getValueByIndex(w) + "->");
+                    // 标记已访问
+                    isVisited[w] = true;
+                    // 入队列
+                    queue.addLast(w);
+                }
+                // 以u为前驱点找w后面的下一个邻接点
+                // 体现出广度优先
+                w = getNextNeighbor(u, w);
+            }
+        }
+    }
+
+    /**
+     * 遍历所有节点进行广度优先搜索
+     */
+    private void bfs(){
+        isVisited = new boolean[5];
+        // bfs遍历
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                bfs(isVisited, i);
+            }
+        }
+    }
+
+    /**
+     * 对dfs进行重载
+     * 遍历所有节点进行dfs
+     */
+    private void dfs() {
+        isVisited = new boolean[5];
+        // 遍历所有节点进行dfs
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
+    }
+
+
+    /**
      * 插入顶点
      *
      * @param vertex 顶点
@@ -68,8 +212,9 @@ public class Graph {
 
     /**
      * 插入边
-     * @param v1 表示第几个顶点的下标
-     * @param v2 表示连接的点的下标
+     *
+     * @param v1     表示第几个顶点的下标
+     * @param v2     表示连接的点的下标
      * @param weight 权值
      */
     public void insertEdge(int v1, int v2, int weight) {
@@ -83,7 +228,7 @@ public class Graph {
     /**
      * @return 返回顶点的个数
      */
-    public int getNumOfVertex(){
+    public int getNumOfVertex() {
         return vertexList.size();
     }
 
@@ -91,7 +236,7 @@ public class Graph {
     /**
      * @return 返回边的个数
      */
-    public int getNumOfEdges(){
+    public int getNumOfEdges() {
         return numOfEdges;
     }
 
@@ -107,7 +252,7 @@ public class Graph {
     /**
      * 展示图
      */
-    public void showGraph(){
+    public void showGraph() {
         for (int[] edge : edges) {
             System.out.println(Arrays.toString(edge));
         }
